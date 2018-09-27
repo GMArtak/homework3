8076066ab04786378e4441ec3e5e7f1ff96e99d6
GMArtak <43244363+GMArtak@users.noreply.github.com>
2018-09-27 19:21:04 +0400

    Add files via upload

---
+++ homw3.js
@@ -0,0 +1,72 @@
+const one = function(mek, erku, ereq, chors){
+return mek * erku * ereq / chors;
+}
+
+
+const two = function ()
+{
+return 11 * 11 / 1
+}
+
+const three = function (a, b){
+console.log( "I'm a function which isn't going to do anything " )
+}
+
+const four = function (StringOne, StringTwo, StringThree)
+if(StringOne.length>StringTwo.length && StringOne.length>StringThree.length){
+		return StringOne;
+	}
+	else if(StringTwo.length>StringOne.length && StringTwo.length>StringThree.length){
+		return StringTwo;
+	}
+	else if(StringThree.length>StringTwo.length && StringThree.length>StringOne.length){
+		return StringThree;
+	}
+	
+	const five = function (c,d){
+	if (c===d){return "0";}
+	
+	else if (d>c) {return "-1";}
+	
+	else if (c>d) { return "1"}
+	}
+	
+	const six = function (e,f){
+	const bazmapatkel = e*f;
+	return bazmapatkel;
+	}
+	
+	const seven = function (j,h){
+	const bajanel = j/h;
+	return bajanel;
+	
+	const triangleArea = function(base, height) {
+  return seven(six(base, height),2) ;
+  
+};
+const numLength = function(o){
+	l = o+"";
+	return l.length;
+}
+const ten = function (q,w,e) {
+	const theSum = q.length + w.length 
+	if (theSum>e) {
+	return 1;
+}
+	else {
+	return -1; 
+}
+};	
+const runStuff = function (z,x,v) {
+	if (z==="rectangle") {
+	return six(x,v);
+}
+	else if (z==="triangle") {
+	return triangleArea(x,v);
+}
+	else {
+	return -1;	
+}
+};
+	
+	
